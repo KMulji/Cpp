cbd2bef2aa54115836f5f3eeaf6d1f578151a0f8
Kyan Mulji <muljikyan@gmail.com>
2023-10-25 09:12:36 -0400

    object orientation

---
+++ ObjectOrientation/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+/*
+    Principles of object orientation
+    1. Abstraction -> Allows people to use it without knowing how a class works.
+    2. Encapsulation -> data hiding.
+    3. Inheritance
+*/
+int main()
+{
+    return 0;
+}
