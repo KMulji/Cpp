da050dd08bcbd910042bcf5d56c7a5920aa07e50
Kyan Mulji <muljikyan@gmail.com>
2023-11-06 04:31:21 -0500

    polymorphism done

--- polymorphism/main.cpp
+++ polymorphism/main.cpp
@@ -27,13 +27,13 @@ class Base
 public:
     /*
         lets assume we have a base class with fun function and a child class
-        which inherits from base. Child also has a fun function. When we create a 
+        which inherits from base. Child also has a fun function. When we create a
         pointer of base class and point it to a object of derived class, and we call
-        function fun. It will call the one in the base class. When we add virtual to the 
+        function fun. It will call the one in the base class. When we add virtual to the
         fun function in base and when the pointer calls fun it will call the derived function.
-        
+
     */
-    virtual void  fun()
+    virtual void fun()
     {
         std::cout << "Fun of Base" << std::endl;
     }
@@ -46,10 +46,119 @@ public:
         std::cout << "Fun of Derived" << std::endl;
     }
 };
+class Car
+{
+public:
+    virtual void start() = 0; // pure virtual function, means derived classes must overide this.
+
+    virtual void stop()
+    {
+        std::cout << "Car Stopped" << std::endl;
+    }
+};
+class Innova : public Car
+{
+public:
+    void start()
+    {
+        std::cout << "Innova Started" << std::endl;
+    }
+    void stop()
+    {
+        std::cout << "Innova Started" << std::endl;
+    }
+};
+class Swift : public Car
+{
+public:
+    void start()
+    {
+        std::cout << "Swift Started" << std::endl;
+    }
+    void stop()
+    {
+        std::cout << "Swift Started" << std::endl;
+    };
+};
+/*
+    Abstract classes
+    student activity
+*/
+/*
+    This shape class is an example of an abstract class called an interface.
+*/
+class Shape
+{
+public:
+    virtual float Area() = 0;
+    virtual float Perimeter() = 0;
+};
+
+class Rectangle : public Shape
+{
+private:
+    float length;
+    float breadth;
+
+public:
+    Rectangle();
+    Rectangle(float l, float b);
+    float Area();
+    float Perimeter();
+};
+class Circle : public Shape
+{
+private:
+    float Radius;
+
+public:
+    Circle();
+    Circle(float r);
+    float Area();
+    float Perimeter();
+};
+Circle::Circle()
+{
+    this->Radius = 1.f;
+}
+Circle::Circle(float r)
+{
+    this->Radius = r;
+}
+float Circle::Area()
+{
+    return 3.142f * this->Radius * this->Radius;
+}
+float Circle::Perimeter()
+{
+    return 2 * (3.142f * Radius);
+}
+Rectangle::Rectangle()
+{
+    length = 1;
+    breadth = 1;
+}
+Rectangle::Rectangle(float l, float b)
+{
+    this->length = l;
+    this->breadth = b;
+}
+float Rectangle::Area()
+{
+    return this->length * this->breadth;
+}
+float Rectangle::Perimeter()
+{
+    return 2 * (this->length * this->breadth);
+}
 int main()
 {
-    Base *p;
-    Derived d=Derived();
-    p=&d;
-    p->fun();
+    Shape *s;
+    s = new Rectangle(2.f, 2.f);
+    std::cout << "Perimeter is" << s->Perimeter() << std::endl;
+    std::cout << "Area is" << s->Area() << std::endl;
+
+    s = new Circle(2.f);
+    std::cout << "Circle Perimeter is" << s->Perimeter() << std::endl;
+    std::cout << "Circle Area is" << s->Area() << std::endl;
 }
