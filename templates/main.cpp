dcc1d54c25cb9a8584fb976ca80dfbcee3cc64e2
KMulji <muljikyan@gmail.com>
2023-11-07 18:13:22 +0300

    templates complete

--- templates/main.cpp
+++ templates/main.cpp
@@ -18,18 +18,72 @@ T add(T x, R y)
 {
     return x + y;
 }
-template<class T>
+template <class T>
 class Stack
 {
 private:
-    T s[10];
-    T top;
+    T *s;
+    int top;
+    int size;
 
 public:
+    Stack(int size);
     void push(T x);
     T pop();
 };
+
+template <class T>
+Stack<T>::Stack(int size)
+{
+
+    this->size = size;
+    this->top = -1;
+    this->s = new T[this->size];
+}
+template <class T>
+void Stack<T>::push(T x)
+{
+    if (top == this->size-1)
+    {
+        throw std::string("Stack is full");
+        
+        return;
+    }
+    top++;
+    this->s[top] = x;
+    
+}
+template <class T>
+T Stack<T>::pop()
+{
+    
+    if (top == -1)
+    {
+        throw std::string("Stack is empty");
+        return -1;
+    }
+    T temp = this->s[top];
+    this->top--;
+    return temp;
+}
 int main()
 {
+    Stack<int> s = Stack<int>(5);
+    try{
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        s.push(4);
+        s.push(5);
+        std::cout<<s.pop()<<std::endl;
+        std::cout<<s.pop()<<std::endl;
+        std::cout<<s.pop()<<std::endl;
+        std::cout<<s.pop()<<std::endl;
+        std::cout<<s.pop()<<std::endl;
+        std::cout<<s.pop()<<std::endl;
+    }catch(std::string s){
+        std::cout<<s<<std::endl;
+    }
+   
     return 0;
 }
