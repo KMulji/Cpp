5fe5c95c7694ba01e682fd14146d72a3f32042e8
KMulji <muljikyan@gmail.com>
2023-11-07 18:34:36 +0300

    templates complete

--- templates/main.cpp
+++ templates/main.cpp
@@ -25,6 +25,7 @@ private:
     T *s;
     int top;
     int size;
+    int length;
 
 public:
     Stack(int size);
@@ -37,6 +38,7 @@ Stack<T>::Stack(int size)
 {
 
     this->size = size;
+
     this->top = -1;
     this->s = new T[this->size];
 }
@@ -46,44 +48,47 @@ void Stack<T>::push(T x)
     if (top == this->size-1)
     {
         throw std::string("Stack is full");
-        
+
         return;
     }
     top++;
     this->s[top] = x;
-    
 }
 template <class T>
 T Stack<T>::pop()
 {
-    
+
     if (top == -1)
     {
         throw std::string("Stack is empty");
         return -1;
     }
+    
     T temp = this->s[top];
-    this->top--;
+    top--;
     return temp;
 }
 int main()
 {
     Stack<int> s = Stack<int>(5);
-    try{
+    try
+    {
         s.push(1);
         s.push(2);
         s.push(3);
         s.push(4);
         s.push(5);
+        
         std::cout<<s.pop()<<std::endl;
         std::cout<<s.pop()<<std::endl;
         std::cout<<s.pop()<<std::endl;
         std::cout<<s.pop()<<std::endl;
         std::cout<<s.pop()<<std::endl;
-        std::cout<<s.pop()<<std::endl;
-    }catch(std::string s){
-        std::cout<<s<<std::endl;
     }
-   
+    catch (std::string s)
+    {
+        std::cout << s << std::endl;
+    }
+
     return 0;
 }
