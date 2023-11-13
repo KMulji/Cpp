9a5dad658fe21dc6a42a905e24c87ad2694f2f23
KMulji <muljikyan@gmail.com>
2023-11-13 13:11:49 +0300

    streams done 1

--- streams/main.cpp
+++ streams/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
 /*
     Streams -> flow of data.
 */
@@ -11,31 +12,32 @@ class Student
 {
 public:
     Student();
-    Student(std::string name,int rollnum,std::string branch);
+    Student(std::string name, int rollnum, std::string branch);
     std::string name;
     int rollNum;
     std::string branch;
     friend std::ofstream &operator<<(std::ofstream &ofs, Student &s);
     friend std::ifstream &operator>>(std::ifstream &ifs, Student &s);
 };
-Student::Student(){;}
+Student::Student() { ; }
 std::ofstream &operator<<(std::ofstream &ofs, Student &s)
 {
     ofs << s.name << std::endl;
     ofs << s.rollNum << std::endl;
-    ofs << s.branch <<std::endl;
+    ofs << s.branch << std::endl;
     return ofs;
 }
-Student::Student(std::string name,int rollnum,std::string branch){
-    this->name=name;
-    this->rollNum=rollNum;
-    this->branch=branch;
-}
-std::ifstream& operator>>(std::ifstream &ifs, Student &s)
+Student::Student(std::string name, int rollnum, std::string branch)
 {
-    ifs>>s.name;
-    ifs>>s.rollNum;
-    ifs>>s.branch;
+    this->name = name;
+    this->rollNum = rollNum;
+    this->branch = branch;
+}
+std::ifstream &operator>>(std::ifstream &ifs, Student &s)
+{
+    ifs >> s.name;
+    ifs >> s.rollNum;
+    ifs >> s.branch;
 
     return ifs;
 }
@@ -56,21 +58,91 @@ void Display()
     std::cout << "number 2 " << number2 << std::endl;
     ifs.close();
 }
+/*Student Exercise*/
+
+class List
+{
+private:
+    std::string Name;
+    float Quantity;
+    float Price;
+
+public:
+    List();
+    List(std::string name, float Quantity, float Price);
+    std::string getName() { return this->Name; }
+    float getQuantity() { return this->Quantity; }
+    float getPrice() { return this->Price; }
+    friend std::ofstream &operator<<(std::ofstream &of, List &L);
+    friend std::ifstream &operator>>(std::ifstream &ifs, List &L);
+    friend std::ostream &operator<<(std::ostream &os, List &L);
+};
+List::List() { ; }
+List::List(std::string name, float Quantity, float Price)
+{
+    this->Name = name;
+    this->Quantity = Quantity;
+    this->Price = Price;
+}
+std::ofstream &operator<<(std::ofstream &of, List &L)
+{
+    of << L.Name << std::endl;
+    of << L.Quantity << std::endl;
+    of << L.Price << std::endl;
+    return of;
+}
+std::ifstream &operator>>(std::ifstream &ifs, List &L)
+{
+    ifs >> L.Name;
+    ifs >> L.Quantity;
+    ifs >> L.Price;
+    return ifs;
+}
+std::ostream &operator<<(std::ostream &os, List &L)
+{
+    std::cout << L.Name << std::endl;
+    std::cout << L.Quantity << std::endl;
+    std::cout << L.Price << std::endl;
+
+    return os;
+}
 int main()
 {
-    Student s1 = Student("Kyan",10,"Nairobi");
-    std::ofstream o("example.txt");
-    o<<s1;
+    int n;
+    std::vector<List> vtr;
+    std::cout << "Enter number of items on your list: ";
+    std::cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        std::string s;
+        float q;
+        float p;
+        std::cin >> s >> q >> p;
+        List temp = List(s, q, p);
+        vtr.push_back(temp);
+    }
 
-    Student s2 = Student();
-    std::ifstream i("example.txt");
-    i>>s2;
+    std::ofstream ofs("exrs.txt");
 
-    std::cout<<s2.name<<std::endl;
-    std::cout<<s2.rollNum<<std::endl;
-    std::cout<<s2.branch<<std::endl;
-    
-    
+    for (int i = 0; i < vtr.size(); i++)
+    {
+        ofs << vtr[i].getName() << std::endl;
+        ofs << vtr[i].getQuantity() << std::endl;
+        ofs << vtr[i].getPrice() << std::endl;
+    }
+    ofs.close();
+
+    std::cout << "Output" << std::endl;
+    std::ifstream ifs("exrs.txt");
+    List item;
+    for (int i = 0; i < n; i++)
+    {
+        ifs >> item;
+        std::cout << item << std::endl;
+    }
+
+    ifs.close();
+
     return 0;
 }
 /*serialization*/
