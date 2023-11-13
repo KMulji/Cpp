9b2dfd1d9b40a94789f1881d1c92947125912f26
KMulji <muljikyan@gmail.com>
2023-11-13 15:05:28 +0300

    stl done

---
+++ stl/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+/*
+    STL
+    1. vector -> dynamic memory array that can resize and shrink
+    2. list -> doubly linked list
+    3. forward_list -> singly linked list.
+    4. deque -> similar to vector and supports insertion and deletion at both ends.
+    5. priority queue -> For heap data structure. e.g max and min heap
+    6. stack
+    7. set -> contains only unique elements
+    8. multi_set -> like a set but allows duplicate
+    9. maps -> store key value pair.
+    10. multi_map -> duplicate keys but value should be different.
+*/
+int main()
+{
+    return 0;
+}
